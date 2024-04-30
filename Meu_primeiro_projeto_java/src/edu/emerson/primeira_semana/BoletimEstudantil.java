6ac291c9caebb359ae2adc374070be5191349297
Emerson <emerson.ricardo153@gmail.com>
2024-04-30 18:51:53 -0300

    Subindo primeiros arquivos

---
+++ Meu_primeiro_projeto_java/src/edu/emerson/primeira_semana/BoletimEstudantil.java
@@ -0,0 +1,20 @@
+package edu.emerson.primeira_semana;
+
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        
+        int mediaFinal = 4;
+
+        if (mediaFinal<6)
+            System.out.println("Reprovado");
+    
+        else if(mediaFinal==6)
+            System.out.println("Prova Minerva");
+    
+        else
+            System.out.println("Aprovado");
+        
+    }
+
+}
---
+++ Meu_primeiro_projeto_java/src/edu/emerson/primeira_semana/MinhaClasse.java
@@ -0,0 +1,31 @@
+
+// Podemos realizar importações apartir de diretorios usando:
+package edu.emerson.primeira_semana;
+
+
+// Sempre declarar as variavel conforme o nome do arquivo java.
+public class MinhaClasse {
+
+    public static void main (String [] args) {
+       
+        // Sempre declarar o tipo da variavel antes do seu primeiro uso:
+        String primeiroNome = "Emerson";
+        String segundoNome = "Ricardo";
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+
+        // Maneira pra realizar uma impressão usando:
+        System.out.println(nomeCompleto);
+    }
+
+
+    // Realizando a concatenação dos objetos:
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    
+    return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+} 
+
+
+
